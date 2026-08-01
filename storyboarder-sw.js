17fa6b9d08be40942fb11c64d7015f54bc08dfb1
hanksleekorea-boop <hanksleekorea@gmail.com>
2026-08-01 17:05:29 +0700

    Deploy Storyboarder UX Complete v0.40.0

---
+++ storyboarder-sw.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var CACHE = 'storyboarder-v0.40.0';
+var CORE = ['./', './index.html', './onbo.html', './app.css', './app.js', './storyboarder.webmanifest', './storyboarder-icon.svg'];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(CACHE).then(function (cache) {
+    return Promise.all(CORE.map(function (url) {
+      return cache.add(url).catch(function () { return null; });
+    }));
+  }).then(function () { return self.skipWaiting(); }));
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (key) { return key !== CACHE; }).map(function (key) { return caches.delete(key); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(function (response) {
+    var copy = response.clone();
+    caches.open(CACHE).then(function (cache) { cache.put(event.request, copy); });
+    return response;
+  }).catch(function () {
+    return caches.match(event.request).then(function (cached) {
+      return cached || caches.match('./onbo.html') || caches.match('./index.html');
+    });
+  }));
+});
